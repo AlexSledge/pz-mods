96bb1933dbbdf3107fd97ef01c857386a94f2adb
Noir <noirjasper@gmail.com>
2022-01-01 02:29:23 -0600

    Gunlight

--- NoirFireArms/media/lua/client/Noir_Toggles.lua
+++ NoirFireArms/media/lua/client/Noir_Toggles.lua
@@ -1,26 +1,29 @@
-availableFireModes = {}
+local availableFireModes = {}
+
+local gunLights = {TacticalLight="",ShotgunTacticalLight=""}
 
 local function ToggleFireMode(key)
 
-    if getSpecificPlayer(0) == nil then return end
+    local player    = getSpecificPlayer(0)
+    if player == nil then return end
     -- LALT = 56
     if(key == 56) then
         availableFireModes = {}
-        local player	= getSpecificPlayer(0)
-		local weapon	= player:getPrimaryHandItem()
+        player	= getSpecificPlayer(0)
+		local item	= player:getPrimaryHandItem()
 
-        if weapon and instanceof(weapon,"HandWeapon") and weapon:isAimedFirearm() then
+        if item and instanceof(item,"HandWeapon") and player:isAiming() then
 
-			local currentMode= weapon:getFireMode()
+			local currentMode= item:getFireMode()
             local currentModeIndex = nil
-			if weapon:getFireModePossibilities() then
-				for i=0, weapon:getFireModePossibilities():size() - 1 do
-					local firemode = weapon:getFireModePossibilities():get(i);
+			if item:getFireModePossibilities() then
+				for i=0, item:getFireModePossibilities():size() - 1 do
+					local firemode = item:getFireModePossibilities():get(i);
                     if(currentMode==firemode) then currentModeIndex = i+1; end
                     table.insert(availableFireModes,firemode);
 				end
                local nextFireMode = nextFireMode(availableFireModes,currentModeIndex)
-               weapon:setFireMode(nextFireMode)
+               item:setFireMode(nextFireMode)
                player:Say(fireModeSay(nextFireMode))
                player:playSound("LightSwitch")
             end
@@ -28,6 +31,36 @@ local function ToggleFireMode(key)
         end
     end
 end
+
+local function ToggleWeaponLight(key)
+
+    local player    = getSpecificPlayer(0)
+    if player == nil then return end
+    if key == getCore():getKey("Equip/Turn On/Off Light Source")  then
+        player = getSpecificPlayer(0);
+        local item = player:getPrimaryHandItem();
+        if item and instanceof(item,"HandWeapon") and player:isAiming() then
+            local sling = item:getSling()
+            if sling then 
+                if gunLights[sling:getType()] ~= nil then
+                    if not item:isActivated() then
+                        item:setTorchCone(true);
+                        item:setLightStrength(1);
+                        item:setLightDistance(8);
+                        item:setActivated(true);
+                    else
+                        item:setTorchCone(false);
+                        item:setLightStrength(0);
+                        item:setLightDistance(0);
+                        item:setActivated(false);
+                    end
+                    player:playSound("LightSwitch")
+                end
+            end
+        end
+    end
+end
+
 
 function nextFireMode(availableFireModes,currentModeIndex)
     local nextFireMode = currentModeIndex+1
@@ -43,3 +76,4 @@ function fireModeSay(fireMode)
 end
 
 Events.OnKeyPressed.Add(ToggleFireMode)
+Events.OnKeyPressed.Add(ToggleWeaponLight)
