2d199d23198de3a686b24b3dfc9dee6ed4de05b0
Noir <noirjasper@gmail.com>
2022-01-01 05:22:40 -0600

    Code improved, weapon have gunlights now

--- NoirFireArms/media/lua/client/Noir_Toggles.lua
+++ NoirFireArms/media/lua/client/Noir_Toggles.lua
@@ -1,19 +1,17 @@
 local availableFireModes = {}
 
-local gunLights = {TacticalLight="",ShotgunTacticalLight=""}
+local gunLights = {GunLight=""}
 
 local function ToggleFireMode(key)
 
     local player    = getSpecificPlayer(0)
     if player == nil then return end
-    -- LALT = 56
-    if(key == 56) then
+    if(key == 56) then     -- Left ALT = 56
         availableFireModes = {}
         player	= getSpecificPlayer(0)
 		local item	= player:getPrimaryHandItem()
 
         if item and instanceof(item,"HandWeapon") and player:isAiming() then
-
 			local currentMode= item:getFireMode()
             local currentModeIndex = nil
 			if item:getFireModePossibilities() then
@@ -24,7 +22,7 @@ local function ToggleFireMode(key)
 				end
                local nextFireMode = nextFireMode(availableFireModes,currentModeIndex)
                item:setFireMode(nextFireMode)
-               player:Say(fireModeSay(nextFireMode))
+               player:Say(showfireMode(nextFireMode))
                player:playSound("LightSwitch")
             end
 
@@ -33,25 +31,17 @@ local function ToggleFireMode(key)
 end
 
 local function ToggleWeaponLight(key)
-
     local player    = getSpecificPlayer(0)
     if player == nil then return end
     if key == getCore():getKey("Equip/Turn On/Off Light Source")  then
-        player = getSpecificPlayer(0);
         local item = player:getPrimaryHandItem();
         if item and instanceof(item,"HandWeapon") and player:isAiming() then
-            local sling = item:getSling()
-            if sling then 
-                if gunLights[sling:getType()] ~= nil then
+            local stock = item:getStock()
+            if stock then
+                if gunLights[stock:getType()] ~= nil then
                     if not item:isActivated() then
-                        item:setTorchCone(true);
-                        item:setLightStrength(1);
-                        item:setLightDistance(8);
                         item:setActivated(true);
                     else
-                        item:setTorchCone(false);
-                        item:setLightStrength(0);
-                        item:setLightDistance(0);
                         item:setActivated(false);
                     end
                     player:playSound("LightSwitch")
@@ -59,6 +49,34 @@ local function ToggleWeaponLight(key)
             end
         end
     end
+end
+
+local function WeaponLightBeam()
+
+    local player = getSpecificPlayer(0)
+    if player == nil then return end
+        local item = player:getPrimaryHandItem();
+        if item and instanceof(item,"HandWeapon") then
+            local stock = item:getStock()
+            if not stock then
+                item:setActivated(false)
+            end
+            if player:isAiming() then
+                if item:isActivated() then
+                    item:setTorchCone(true);
+                    item:setLightStrength(1);
+                    item:setLightDistance(8);
+                else
+                    item:setTorchCone(false);
+                    item:setLightStrength(0);
+                    item:setLightDistance(0);
+                end
+            else
+                item:setTorchCone(false);
+                item:setLightStrength(0);
+                item:setLightDistance(0);
+            end
+        end
 end
 
 
@@ -70,10 +88,11 @@ function nextFireMode(availableFireModes,currentModeIndex)
     return availableFireModes[nextFireMode]
 end
 
-function fireModeSay(fireMode)
+function showfireMode(fireMode)
     if fireMode == "Auto" then return "-----" end
     if fireMode == "Single" then return "-" end
 end
 
 Events.OnKeyPressed.Add(ToggleFireMode)
 Events.OnKeyPressed.Add(ToggleWeaponLight)
+Events.OnPlayerUpdate.Add(WeaponLightBeam)
