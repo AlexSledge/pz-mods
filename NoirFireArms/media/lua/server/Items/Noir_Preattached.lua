38cb6f70f9ad7f38314952b1185d508fd779e151
Noir <noirjasper@gmail.com>
2022-01-06 13:14:01 -0600

    Own Textures

--- NoirFireArms/media/lua/server/Items/Noir_Preattached.lua
+++
@@ -1,6 +0,0 @@
-WeaponUpgrades = {
-    VarmintRifle = {"x2Scope", "x4Scope", "x8Scope", "AmmoStraps", "Sling", "FiberglassStock", "RecoilPad", "IronSight","223Silencer"},
-    HuntingRifle = {"x2Scope", "x4Scope", "x8Scope", "AmmoStraps", "Sling", "FiberglassStock", "RecoilPad","308Silencer"},
-    Shotgun = {"AmmoStraps", "Sling", "ChokeTubeFull", "ChokeTubeImproved","12gaSilencer","GunLight"},
-    Pistol = {"Laser", "RedDot", "IronSight","9mmSilencer","GunLight"}
-}
