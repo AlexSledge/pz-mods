1a55b2213b65fbcddaa7cab076891f3015b081e3
Noir <noirjasper@gmail.com>
2022-01-10 18:32:46 -0600

    GunLight Distributions

--- NoirVanillaFirearms/media/lua/server/Items/Noir_Distributions.lua
+++ NoirVanillaFirearms/media/lua/server/Items/Noir_Distributions.lua
@@ -11,7 +11,7 @@ local armyPawnWeapons = {
     "PawnShopGunsSpecial"
 }
 
-local policeGunStoresWeapons ={ 
+local policeGunStoresWeapons ={
     "GunStoreCounter",
     "GunStoreDisplayCase",
     "GunStoreShelf",
@@ -21,10 +21,12 @@ local policeGunStoresWeapons ={
 for i =1 , #safeHouseWeapons do
     table.insert(ProceduralDistributions.list[safeHouseWeapons[i]].items, "Base.12gaSilencer");
     table.insert(ProceduralDistributions.list[safeHouseWeapons[i]].items, 3);
-    table.insert(ProceduralDistributions.list[armyPawnWeapons[i]].items, "Base.9mmSilencer");
-    table.insert(ProceduralDistributions.list[armyPawnWeapons[i]].items, 3);
-    table.insert(ProceduralDistributions.list[armyPawnWeapons[i]].items, "Base.45Silencer");
-    table.insert(ProceduralDistributions.list[armyPawnWeapons[i]].items, 3);
+    table.insert(ProceduralDistributions.list[safeHouseWeapons[i]].items, "Base.9mmSilencer");
+    table.insert(ProceduralDistributions.list[safeHouseWeapons[i]].items, 3);
+    table.insert(ProceduralDistributions.list[safeHouseWeapons[i]].items, "Base.45Silencer");
+    table.insert(ProceduralDistributions.list[safeHouseWeapons[i]].items, 3);
+    table.insert(ProceduralDistributions.list[safeHouseWeapons[i]].items, "GunLight");
+    table.insert(ProceduralDistributions.list[safeHouseWeapons[i]].items, 3);
 end
 
 for i =1 , #armyPawnWeapons do
@@ -39,6 +41,8 @@ for i =1 , #armyPawnWeapons do
     table.insert(ProceduralDistributions.list[armyPawnWeapons[i]].items, 10);
     table.insert(ProceduralDistributions.list[armyPawnWeapons[i]].items, "Base.12gaSilencer");
     table.insert(ProceduralDistributions.list[armyPawnWeapons[i]].items, 10);
+    table.insert(ProceduralDistributions.list[armyPawnWeapons[i]].items, "GunLight");
+    table.insert(ProceduralDistributions.list[armyPawnWeapons[i]].items, 10);
 end
 
 for i =1 , #policeGunStoresWeapons do
@@ -48,6 +52,8 @@ for i =1 , #policeGunStoresWeapons do
     table.insert(ProceduralDistributions.list[policeGunStoresWeapons[i]].items, 5);
     table.insert(ProceduralDistributions.list[policeGunStoresWeapons[i]].items, "Base.12gaSilencer");
     table.insert(ProceduralDistributions.list[policeGunStoresWeapons[i]].items, 5);
+    table.insert(ProceduralDistributions.list[policeGunStoresWeapons[i]].items, "GunLight");
+    table.insert(ProceduralDistributions.list[policeGunStoresWeapons[i]].items, 5);
 end
 
 
@@ -55,19 +61,30 @@ table.insert(SuburbsDistributions["GunCache1"]["GunBox"].items, "Base.9mmSilence
 table.insert(SuburbsDistributions["GunCache1"]["GunBox"].items, 5);
 table.insert(SuburbsDistributions["GunCache1"]["GunBox"].items, "Base.45Silencer");
 table.insert(SuburbsDistributions["GunCache1"]["GunBox"].items, 5);
+table.insert(SuburbsDistributions["GunCache1"]["GunBox"].items, "GunLight");
+table.insert(SuburbsDistributions["GunCache1"]["GunBox"].items, 5);
 
 table.insert(SuburbsDistributions["GunCache2"]["GunBox"].items, "Base.9mmSilencer");
 table.insert(SuburbsDistributions["GunCache2"]["GunBox"].items, 5);
 table.insert(SuburbsDistributions["GunCache2"]["GunBox"].items, "Base.45Silencer");
+table.insert(SuburbsDistributions["GunCache2"]["GunBox"].items, 5);
+table.insert(SuburbsDistributions["GunCache2"]["GunBox"].items, "GunLight");
 table.insert(SuburbsDistributions["GunCache2"]["GunBox"].items, 5);
 
 table.insert(SuburbsDistributions["GunCache2"]["Bag_DuffelBagTINT"].items, "Base.9mmSilencer");
 table.insert(SuburbsDistributions["GunCache2"]["Bag_DuffelBagTINT"].items, 5);
 table.insert(SuburbsDistributions["GunCache2"]["Bag_DuffelBagTINT"].items, "Base.45Silencer");
 table.insert(SuburbsDistributions["GunCache2"]["Bag_DuffelBagTINT"].items, 5);
-
-table.insert(SuburbsDistributions["ShotgunCache2"]["ShotgunBox"].items, "Base.12gaSilencer");
-table.insert(SuburbsDistributions["ShotgunCache2"]["ShotgunBox"].items, 5);
+table.insert(SuburbsDistributions["GunCache2"]["Bag_DuffelBagTINT"].items, "GunLight");
+table.insert(SuburbsDistributions["GunCache2"]["Bag_DuffelBagTINT"].items, 5);
 
 table.insert(SuburbsDistributions["ShotgunCache1"]["ShotgunBox"].items, "Base.12gaSilencer");
 table.insert(SuburbsDistributions["ShotgunCache1"]["ShotgunBox"].items, 5);
+table.insert(SuburbsDistributions["ShotgunCache1"]["ShotgunBox"].items, "GunLight");
+table.insert(SuburbsDistributions["ShotgunCache1"]["ShotgunBox"].items, 5);
+
+table.insert(SuburbsDistributions["ShotgunCache2"]["ShotgunBox"].items, "Base.12gaSilencer");
+table.insert(SuburbsDistributions["ShotgunCache2"]["ShotgunBox"].items, 5);
+table.insert(SuburbsDistributions["ShotgunCache2"]["ShotgunBox"].items, "GunLight");
+table.insert(SuburbsDistributions["ShotgunCache2"]["ShotgunBox"].items, 5);
+
--- NoirVanillaFirearms/media/lua/server/Items/Noir_Preattached.lua
+++ NoirVanillaFirearms/media/lua/server/Items/Noir_Preattached.lua
@@ -3,5 +3,5 @@ WeaponUpgrades = {
     HuntingRifle = {"x2Scope", "x4Scope", "x8Scope", "AmmoStraps", "Sling", "FiberglassStock", "RecoilPad"},
     Shotgun = {"AmmoStraps", "Sling", "ChokeTubeFull", "ChokeTubeImproved","12gaSilencer"},
     AssaultRifle = {"RedDot","GunLight","223Silencer"},
-    Pistol = {"Laser", "RedDot", "IronSight","9mmSilencer",}
+    Pistol = {"Laser", "RedDot", "IronSight","9mmSilencer","GunLight"}
 }
