9059ea6a7d0ae2b305abe56ebb4bd749cdafd2a3
unknown <noirjasper@gmail.com>
2022-04-09 22:39:08 -0500

    Renamed

--- NoirBackpackAttachments/media/lua/client/TimedActions/NATT_ISMoveablesAction.lua
+++ NoirBackpackAttachments/media/lua/client/TimedActions/NATT_ISMoveablesAction.lua
@@ -1,9 +1,9 @@
-local originalStart= ISMoveablesAction.start;
+local oldStart= ISMoveablesAction.start;
 function ISMoveablesAction:start()
 	if self.origSpriteName and string.find(self.origSpriteName,"sleepingbag") then 
         self:setActionAnim("Loot")
         self.character:SetVariable("LootPosition", "Low")
         self:setOverrideHandModels(nil, nil)
     end
-	originalStart(self)
+	oldStart(self)
 end
