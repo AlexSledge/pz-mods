6fbb40d359fad28ca2b696a5d2950d5fb2c7ed8f
unknown <noirjasper@gmail.com>
2022-04-12 10:03:13 -0500

    Stuff

--- NoirFirearms/media/lua/server/Vehicles/zFRUsedCars.lua
+++
@@ -1,8 +0,0 @@
-require 'Vehicles/VehicleDistributions'
-
-if getActivatedMods():contains("FRUsedCars") then 
-    table.insert(VehicleDistributions["MilitaryGearTrunk"].items, "Base.AA12");
-    table.insert(VehicleDistributions["MilitaryGearTrunk"].items, 2);
-    table.insert(VehicleDistributions["MilitaryGearTrunk"].items, "Base.AA12Clip");
-    table.insert(VehicleDistributions["MilitaryGearTrunk"].items, 2);
-end    
--- NoirJarsAndLids/media/lua/client/NJL_Recipe.lua
+++
@@ -1,5 +0,0 @@
-function recipe_Open_Jar(items, result, player)
-	local inv = player:getInventory();
-	inv:AddItem("EmptyJar");
-	inv:AddItem("JarLid");
-end
